062787b5e62da06ec605cc5efc5ddcae72e3457f
QuestickAG <azat.gilmetdinov98@mail.ru>
2020-06-21 15:10:09 +0300

    firstcommit

---
+++ Models/RoleType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Qualification.Models
+{
+    public enum RoleType
+    {
+        /// <summary>
+        /// Работник.
+        /// </summary>
+        Employee = 1,
+
+        /// <summary>
+        /// Работодатель.
+        /// </summary>
+        Employer = 2
+    }
+}
